eb2e6ab912fbea71a9a71f2573bcffbe659ff26f
Creeper Lv <creeperlv38@outlook.com>
2020-03-13 22:59:35 +0800

    Updated Help and added multiple language support on Help Document.

--- ExtGit/Program.cs
+++ ExtGit/Program.cs
@@ -8,7 +8,7 @@ namespace ExtGit
 {
     class Program
     {
-        static Operation CurrentOperation= Operation.None;
+        static Operation CurrentOperation = Operation.None;
         static Version ShellVersion = new Version(1, 0, 1, 0);
         static void Main(string[] args)
         {
@@ -67,12 +67,12 @@ namespace ExtGit
                         CurrentOperation = Operation.FunctionTest;
                         break;
                     default:
-                        Console.WriteLine("Unknown parameter:"+args[i]);
+                        Console.WriteLine("Unknown parameter:" + args[i]);
                         break;
                 }
             }
-            Console.WriteLine(Language.CurrentLanguage.Get("TITLE0","ExtGit - Designed for commit large file without GIT-LFS to host platforms."));
-            Console.WriteLine(Language.CurrentLanguage.Get("PREVIEW00","This software still in an early preview"));
+            Console.WriteLine(Language.CurrentLanguage.Get("TITLE0", "ExtGit - Designed for commit large file without GIT-LFS to host platforms."));
+            Console.WriteLine(Language.CurrentLanguage.Get("PREVIEW00", "This software still in an early preview"));
             Console.WriteLine("");
             Console.WriteLine(Language.CurrentLanguage.Get("TITLE1", "Designed for What Happened to Site-13?"));
             switch (CurrentOperation)
@@ -82,9 +82,9 @@ namespace ExtGit
                         //try
                         //{
 
-                            double prog = 0.0;
-                            Repo r = new Repo(new DirectoryInfo("./").FullName);
-                            r.Commit(ref prog);
+                        double prog = 0.0;
+                        Repo r = new Repo(new DirectoryInfo("./").FullName);
+                        r.Commit(ref prog);
                         //}
                         //catch (Exception e)
                         //{
@@ -142,10 +142,10 @@ namespace ExtGit
         private static void ShowVersion()
         {
             Console.WriteLine("");
-            Console.WriteLine(Language.CurrentLanguage.Get("VERINFOSTR00","ExtGit Components:"));
+            Console.WriteLine(Language.CurrentLanguage.Get("VERINFOSTR00", "ExtGit Components:"));
             Console.WriteLine("");
-            Console.WriteLine("\tShell Version:\t" + ShellVersion.ToString()+$"({ShellVersion.Build})");
-            Console.WriteLine("\tCore Version:\t" + CoreLib.CurrentCore.CoreVersion.ToString() + $"({CoreLib.CurrentCore.CoreVersion.Build})");
+            Console.WriteLine($"\t{Language.CurrentLanguage.Get("VERINFOSTR03", "Shell Version:")}\t" + ShellVersion.ToString() + $"({ShellVersion.Build})");
+            Console.WriteLine($"\t{Language.CurrentLanguage.Get("VERINFOSTR04", "Core Version:")}\t" + CoreLib.CurrentCore.CoreVersion.ToString() + $"({CoreLib.CurrentCore.CoreVersion.Build})");
             Console.WriteLine("");
             Console.WriteLine($"\t{Language.CurrentLanguage.Get("VERINFOSTR01", "Target Config:")}\t" + Repo.CurrentConfigVer.ToString() + $"({Repo.CurrentConfigVer.Build})");
             Console.WriteLine($"\t{Language.CurrentLanguage.Get("VERINFOSTR02", "Min Config:")}\t" + Repo.MinConfigVer.ToString() + $"({Repo.MinConfigVer.Build})");
@@ -153,6 +153,14 @@ namespace ExtGit
 
         static void ShowHelp()
         {
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(Language.CurrentLanguage.Get($"HELP{i:D2}"));
+            }
+        }
+        public static void ShowOldHelp()
+        {
+
             Console.WriteLine("extgit [Options|Operations]");
             Console.WriteLine("Operations:");
             Console.WriteLine("\t-C|COMMIT\tCommit changes to ExtGit workload.");
@@ -167,6 +175,6 @@ namespace ExtGit
     }
     enum Operation
     {
-        Commit,Checkout,None,Help,Version,Create,Graft,FunctionTest
+        Commit, Checkout, None, Help, Version, Create, Graft, FunctionTest
     }
 }
